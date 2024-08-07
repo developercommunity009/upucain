a24035bc423e91153d1ed60d4e5f5e184d3d5262
developercommunity009@gmail.com <developercommunity009@gmail.com>
2024-08-07 12:08:25 +0300

    Update constant.js

--- client/src/constant.js
+++ client/src/constant.js
@@ -1,3 +1,3 @@
-export const BACKEND_URL ='https://upucain-k8bk.vercel.app/'
+export const BACKEND_URL =`https://upucain-k8bk.vercel.app/`
 // export const BACKEND_URL = 'https://inhertly-backend.vercel.app'
 
